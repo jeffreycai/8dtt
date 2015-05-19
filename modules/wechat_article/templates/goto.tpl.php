964695bea00b3abb1af1d6d391e25cbb894ea79f
jeffreycai <jeffreycaizhenyuan@gmail.com>
2015-05-19 14:01:31 +1000

    minor

--- modules/wechat_article/templates/goto.tpl.php
+++ modules/wechat_article/templates/goto.tpl.php
@@ -6,8 +6,6 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   
-  <!--<meta http-equiv="refresh" content="1; url=<?php echo $article->getUrl() ?>" />-->
-
   <title><?php echo $article->getTitle() ?> :: <?php echo $settings['sitename'] ?></title>
 
 </head>
