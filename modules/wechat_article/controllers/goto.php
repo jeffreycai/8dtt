583a9fca8c4953e31880334373a84f6e151bdc6d
jeffreycai <jeffreycaizhenyuan@gmail.com>
2015-05-28 09:46:32 +1000

    minor

--- modules/wechat_article/controllers/goto.php
+++ modules/wechat_article/controllers/goto.php
@@ -1,18 +1,21 @@
 <?php
+
+
+
 $id = isset($vars[1]) ? $vars[1] : null;
 $article = WechatArticle::findById($id);
 if ($article) {
   $article->setClick($article->getClick() + 1);
   $article->save();
   
+  HTML::forward($article->getUrl());
   
   $html = new HTML();
   $html->renderOut('wechat_article/goto', array(
       'article' => $article
   ));
   
-  
-//  HTML::forward($article->getUrl());
 } else {
-  HTML::forward('site/404');
+  dispatch('site/404');
+  exit();
 }
